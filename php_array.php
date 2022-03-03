60feb3593c45ac75a96a5a695aec00d6dac56b66
rameshre <zycon1mac@gmail.com>
2022-03-03 14:47:48 +0530

    table

---
+++ php_array.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<?php
+$age=array("Peter"=>35,"Ben"=>37,"joe"=>43);
+arsort($age);
+foreach($age as $x=>$_value)
+{
+echo "key=".$x.",value=".$_value;
+echo "<br>";
+}
+krsort($age);
+foreach($age as $y=>$y_value)
+{
+echo "key=".$y.",value=".$y_value;
+echo "<br>";
+}
+?>
+</body>
+</html>
